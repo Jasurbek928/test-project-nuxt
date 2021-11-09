968ee647ce37fac332d81609e1b6a1a473e01a18
Jasurbek Muhammadiyev <jasurbekmuhammadiyev8@gmail.com>
2021-11-10 00:56:43 +0500

    user-profile change

--- pages/index.vue
+++ pages/index.vue
@@ -1,96 +1,85 @@
 <template>
-  <div class="min-h-full">
- {{users}}
-    <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
-        <div class="px-4 py-6 sm:px-0">
-          <!-- This example requires Tailwind CSS v2.0+ -->
-          <div class="flex flex-col">
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                  <table class="min-w-full divide-y divide-gray-200">
-                    <thead class="bg-gray-50">
-                      <tr>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >T/r</th>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >Name</th>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >UserName</th>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >Email</th>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >street</th>
-                         <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >suite</th>
-                         <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >city</th>
-
-                      </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                      <tr v-for="(user,idx) in users" :key="idx" @click="UserProfile">
-                        <td class="px-6 py-4 whitespace-nowrap">
-                         {{user.id}}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                         {{user.name}}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                          {{user.username}}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {{user.email}}</td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {{ user.address.street}}</td>
-                      <td class="px-6 py-4 whitespace-nowrap">
-                        {{ user.address.suite }}
-                      </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {{user.address.city}}</td>
-                      </tr>
-                    </tbody>
-                  </table>
+  <!-- component -->
+<div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
+                <div class="align-middle rounded-tl-lg rounded-tr-lg inline-block w-full py-4 overflow-hidden bg-white shadow-lg px-12">
+                    <div class="flex justify-between">
+                        <div class="inline-flex border rounded w-7/12 px-2 lg:px-6 h-12 bg-transparent">
+                            <div class="flex flex-wrap items-stretch w-full h-full mb-6 relative">
+                                <div class="flex">
+                                    <span class="flex items-center leading-normal bg-transparent rounded rounded-r-none border border-r-0 border-none lg:px-3 py-2 whitespace-no-wrap text-grey-dark text-sm">
+                                        <svg width="18" height="18" class="w-4 lg:w-auto" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M8.11086 15.2217C12.0381 15.2217 15.2217 12.0381 15.2217 8.11086C15.2217 4.18364 12.0381 1 8.11086 1C4.18364 1 1 4.18364 1 8.11086C1 12.0381 4.18364 15.2217 8.11086 15.2217Z" stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" />
+                                            <path d="M16.9993 16.9993L13.1328 13.1328" stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                    </span>
+                                </div>
+                                <input type="text" class="flex-shrink flex-grow flex-auto leading-normal tracking-wide w-px flex-1 border border-none border-l-0 rounded rounded-l-none px-3 relative focus:outline-none text-xxs lg:text-xs lg:text-base text-gray-500 font-thin" placeholder="Search">
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- /End replace -->
+                <div class="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
+                    <table class="min-w-full">
+                        <thead>
+                            <tr>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">ID</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Name</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Username</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Email</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Street</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">City</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Status</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">Website</th>
+                                <th class="px-6 py-3 border-b-2 border-gray-300"></th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white">
+                                <tr v-for="(user,idx) in users" :key="idx" @click="userProfile">
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                        <div class="flex items-center">
+                                            <div>
+                                                <div class="text-sm leading-5 text-gray-800">{{user.id}}</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                        <div class="text-sm leading-5 text-blue-900">{{user.name}}</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">{{user.username}}</td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">{{user.email}}</td>
 
-      </div>
-    </main>
-  </div>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">{{user.address.street}}</td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">{{user.address.city}}</td>
+                                     <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">
+                                        <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                        <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                        <span class="relative text-xs">active</span>
+                                    </span>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500 text-blue-900 text-sm leading-5">{{user.website}}</td>
+                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                          <nuxt-link :to="`user/${user.id}`"><button  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Profile</button></nuxt-link>
+                        </td>
+                          </tr>
+                        </tbody>
+                    </table>
+                  <div class="sm:flex-1 sm:flex sm:items-center sm:justify-between mt-4 work-sans">
+
+    </div>
+                </div>
+            </div>
+
 </template>
 
 <script>
 export default {
-
-  methods:{
-    UserProfile(){
-      this.$router.push('/userProfile')
-    }
-  },
-
    async asyncData({ $axios }) {
       const users = await $axios.$get(`https://jsonplaceholder.typicode.com/users`)
       return { users }
     },
 }
 </script>
+
+<style>
+
+</style>
---
+++ pages/user/_id.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+     <header class="bg-white shadow">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl  text-gray-900">User : {{$route.params.id}}</h1>
+      </div>
+    </header>
+     <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <!-- Replace with your content -->
+        <div class="px-4 py-6 sm:px-0">
+          <!-- This example requires Tailwind CSS v2.0+ -->
+          <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class=" shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                   Name:
+                  <input type="text" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" v-model="user.name">
+                 <br>
+                  Username:
+                  <input type="text" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" v-model="user.username">
+                  <br>
+                  Email:
+                  <input type="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" v-model="user.email">
+                  
+                  <br/>
+                 Website:
+                  <input type="text" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" v-model="user.website">
+
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /End replace -->
+      </div>
+    </main>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      user:{}
+    }
+  },
+ async mounted(){
+       await this.$axios.get("https://jsonplaceholder.typicode.com/users/" +  this.$route.params.id)
+        .then((res)=> {
+          this.user=res.data
+          console.log(res)
+      })
+    },
+   methods:{
+    userUpdateData(){
+        this.$axios.put("https://jsonplaceholder.typicode.com/users/" +  this.$route.params.id,{...this.user} )
+          .then((res)=>{
+            this.$router.push('/')
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
